66cf24079b814ee15a4b867a5183cb4baaab7497
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-10-04 21:51:24 -0700

    format script.js to calculate time

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+
+// setInterval(realTime, 1000)
+// setInterval(realHour, 1000)
+// setInterval(realMinutes, 1000);
+// setInterval(realSeconds, 1000);
+// setInterval(countdown, 1);
