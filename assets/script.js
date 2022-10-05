c90631d075f2b63c1709f53f6fb14d083b64ad60
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-10-04 23:07:07 -0700

    timer in header to function and symbols to spin

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,17 @@
-$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+// $("#currentDay").text(moment().format("[Today], dddd, MMMM Do YYYY h:mm:ss a"));
+$(document).ready(function () {
 
+let realTime = function () {
+    let currentTime = moment().format('[Today], dddd MMMM Do YYYY h:mm:ss a')
+    $("#currentDay").text(currentTime)
+}
 
-// setInterval(realTime, 1000)
+$( function() {
+    $( "#description" ).selectable();
+  } );
+setInterval(realTime, 1000)
 // setInterval(realHour, 1000)
 // setInterval(realMinutes, 1000);
 // setInterval(realSeconds, 1000);
 // setInterval(countdown, 1);
+})
